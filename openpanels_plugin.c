cf369a27e54acfda7cdbb6ab87e4ba614c16d9d8
Nagy Attila Gabor <mrbig@sneaker.hu>
2011-08-25 10:54:32 +0200

    Plugin cleanup

--- openpanels_plugin.c
+++ openpanels_plugin.c
@@ -1,13 +1,13 @@
 /* Copyright (C) 2009 Nagy Attila Gabor <nagy.attila.gabor@gmail.com>
  *
- *     This file is part of TrackMii.
+ *     This file is part of OpenPanels.
  *
- *  TrackMii is free software: you can redistribute it and/or modify
+ *  OpenPanels is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  TrackMii is distributed in the hope that it will be useful,
+ *  OpenPanels is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
